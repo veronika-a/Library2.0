886f502e88aa3665e7a091be066c2d1caa4ebdfa
veronika-a <andrianika00@gmail.com>
2019-12-08 14:19:33 +0200

    migrations

---
+++ Migrations/201912081005467_fk.cs
@@ -0,0 +1,20 @@
+﻿namespace Library.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fk : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.ReaderCards", "ReaderId");
+            AddForeignKey("dbo.ReaderCards", "ReaderId", "dbo.Readers", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ReaderCards", "ReaderId", "dbo.Readers");
+            DropIndex("dbo.ReaderCards", new[] { "ReaderId" });
+        }
+    }
+}
