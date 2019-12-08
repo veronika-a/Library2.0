7dfbc5e827699c6f218934ad42168d2ba6dec2c4
veronika-a <andrianika00@gmail.com>
2019-12-08 14:19:43 +0200

    Update Chapter.cs

--- Models/Chapter.cs
+++ Models/Chapter.cs
@@ -10,12 +10,13 @@ namespace Library.Models
         [Key]
         [Column("Id")]
         private int id;
-        [ForeignKey("Book")]
         private int bookid;
         private double number;
         private string title;
         private int firstPage;
         private int lastPage;
+        [ForeignKey("bookid")]
+        private Book book;
 
         public int Id { get => id; set => id = value; }
         public double Number { get => number; set => number = value; }
@@ -23,5 +24,6 @@ namespace Library.Models
         public int FirstPage { get => firstPage; set => firstPage = value; }
         public int LastPage { get => lastPage; set => lastPage = value; }
         public int BookId { get => bookid; set => bookid = value; }
+        public Book Book { get => book; set => book = value; }
     }
 }
