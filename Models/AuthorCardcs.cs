7aa27772e1a605a39b07b2aceb15ff1a597618d5
veronika-a <andrianika00@gmail.com>
2019-12-08 14:19:41 +0200

    Update AuthorCardcs.cs

--- Models/AuthorCardcs.cs
+++ Models/AuthorCardcs.cs
@@ -9,13 +9,17 @@ namespace Library.Models
         [Key]
         [Column("Id")]
         private int id;
-        [ForeignKey("Author")]
         private int authorid;
-        [ForeignKey("Book")]
         private int bookid;
+        [ForeignKey("authorid")]
+        private Author author;
+        [ForeignKey("bookid")]
+        private Book book;
 
         public int Id { get => id; set => id = value; }
         public int Authorid { get => authorid; set => authorid = value; }
         public int Bookid { get => bookid; set => bookid = value; }
+        public Book Book { get => book; set => book = value; }
+        public Author Author { get => author; set => author = value; }
     }
 }
