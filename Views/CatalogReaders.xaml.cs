2a60d5eded8f4e70cf1532bd00fb0f528399974f
veronika-a <andrianika00@gmail.com>
2019-12-08 14:18:35 +0200

    Update CatalogReaders.xaml.cs

--- Views/CatalogReaders.xaml.cs
+++ Views/CatalogReaders.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,14 @@ namespace Library.Views
     /// </summary>
     public partial class CatalogReaders : Window
     {
+        
+        CatalogReadersViewModel catalog = new CatalogReadersViewModel();
         public CatalogReaders()
         {
             InitializeComponent();
+            DataContext = catalog;
+
+            catalog.Closing += (s, e) => this.Close();
         }
     }
 }
