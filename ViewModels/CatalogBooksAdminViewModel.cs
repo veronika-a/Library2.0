259beb4a602f2a1e747512d2745742b181561c38
veronika-a <andrianika00@gmail.com>
2019-12-09 13:39:07 +0200

    Create CatalogBooksAdminViewModel.cs

---
+++ ViewModels/CatalogBooksAdminViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Library.Models;
+using Library.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.ViewModels
+{
+    
+    public class CatalogBooksAdminViewModel : INotifyPropertyChanged
+    {
+        //ObservableCollection<UserModel> selectedUsers;
+        public event EventHandler Closing;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        Book _book;
+
+        ObservableCollection<Book> _books;
+        public ObservableCollection<Book> Books
+        {
+            get { return _books; }
+            set
+            {
+                _books = value;
+                OnPropertyChanged(nameof(Books));
+            }
+        }
+        public CatalogBooksAdminViewModel()
+        {
+            Books = new ObservableCollection<Book>(GetBooks());
+        }
+
+        List<Book> GetBooks()
+        {
+            using (MyAppContext appContext = new MyAppContext())
+            {
+                BookRepository bookRepository = new BookRepository(appContext);
+                return (List<Book>)bookRepository.GetAll();
+            }
+        }
+
+        private string _search;
+        public string Search
+        {
+            get { return _search; }
+            set
+            {
+                _search = value;
+                OnPropertyChanged(nameof(Search));
+                Books = new ObservableCollection<Book>(GetBooks().Where(i => i.Title.Contains(Search)));
+                // || i.FirstName.Contains(Search)
+                // || i.SecondName.Contains(Search))); ;
+
+            }
+        }
+    }
+}
