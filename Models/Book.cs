435434dc06852c63049a5fc0d8abc26e1d18caa2
veronika-a <andrianika00@gmail.com>
2019-12-11 18:09:26 +0200

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,14 +15,18 @@ namespace Library.Models
         private int id;
         private string title;
         private string ganre;
-        private string location;
-        private string edition;
+        private string about;
+        private string publisher;
+        private int? publicationDate;
+        private string coverArtist;
 
         public int Id { get => id; set => id = value; }
         public string Title { get => title; set => title = value; }
         public string Ganre { get => ganre; set => ganre = value; }
-        public string Location { get => location; set => location = value; }
-        public string Edition { get => edition; set => edition = value; }
+        public string About { get => about; set => about = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public int? PublicationDate { get => publicationDate; set => publicationDate = value; }
+        public string CoverArtist { get => coverArtist; set => coverArtist = value; }
 
 
         //Author author;
