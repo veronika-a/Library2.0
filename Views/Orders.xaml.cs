4a5f07502af4be5a3bb0ddeab6b191717b82b7c3
veronika-a <andrianika00@gmail.com>
2019-12-14 22:04:43 +0200

    Update Orders.xaml.cs

--- Views/Orders.xaml.cs
+++ Views/Orders.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,15 @@ namespace Library.Views
     /// </summary>
     public partial class Orders : Window
     {
+        OrdersViewModel ordersViewModel = new OrdersViewModel();
         public Orders()
         {
             InitializeComponent();
+            DataContext = ordersViewModel;
+            ordersViewModel.Closing += (s, e) => this.Close();
+
         }
+      
+
     }
 }
